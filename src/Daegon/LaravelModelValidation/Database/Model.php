daefe97fa703c1678684f7c0aa1cbc48da1299bd
Daegon <romsssa@gmail.com>
2015-07-22 18:11:43 +0300

    Init

---
+++ src/Daegon/LaravelModelValidation/Database/Model.php
@@ -0,0 +1,57 @@
+<?php namespace Daegon\LaravelModelValidation\Database;
+
+use Daegon\LaravelModelValidation\Exceptions\ModelValidationFailException;
+use Illuminate\Validation\Validator;
+
+abstract class Model extends \Eloquent
+{
+    public $skipValidation = false;
+    public $throwValidationException = false;
+    protected static $rules = [];
+    protected static $messages = [];
+    public $validation;
+
+    public function __construct(array $attributes = array(), Validator $validator = null)
+    {
+        parent::__construct($attributes);
+
+        $this->validator = $validator ?: \App::make('validator');
+    }
+
+    public function save_(array $options = []){
+        $this->throwValidationException = true;
+        $callResult = parent::save($options);
+        $this->throwValidationException = false;
+        return $callResult;
+    }
+
+    public function update_(array $attributes = []){
+        $this->throwValidationException = true;
+        $callResult = parent::save($attributes);
+        $this->throwValidationException = false;
+        return $callResult;
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function(ValidationEloquent $model) {
+            if($model->skipValidation) {
+                return true;
+            }
+            if(!$model->isValid()) {
+                if($model->throwValidationException) {
+                    throw new ModelValidationFailException('Validation failed', $model->validation->errors);
+                }
+                return false;
+            };
+            return true;
+        });
+    }
+
+    public function isValid() {
+        $this->validation = $this->validator->make($this->attributes, static::$rules, static::$messages);
+        return $this->validation->passes();
+    }
+}
---
+++ src/Daegon/LaravelModelValidation/Exceptions/ModelValidationFailException.php
@@ -0,0 +1,31 @@
+<?php namespace Daegon\LaravelModelValidation\Exceptions;
+
+class ModelValidationFailException extends \Exception {
+
+    /**
+     * @var mixed
+     */
+    protected $errors;
+
+    /**
+     * @param string $message
+     * @param mixed  $errors
+     */
+    function __construct($message, $errors)
+    {
+        $this->errors = $errors;
+
+        parent::__construct($message);
+    }
+
+    /**
+     * Get form validation errors
+     *
+     * @return mixed
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+}
